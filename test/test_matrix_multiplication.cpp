7043f523f3ef47594a9448213c50be5f6ec53c70
Francesco <francesco.faggion.4@gmail.com>
2024-05-31 15:28:36 +0200

    a

--- test/test_matrix_multiplication.cpp
+++ test/test_matrix_multiplication.cpp
@@ -6,6 +6,29 @@
 // ######################### Source code of multiplyMatrices in src/matrix_mult
 
 
+TEST(MatrixMultiplicationTest, TestMultiplyMatrices) {
+    std::vector<std::vector<int>> A = {
+        {1, 2, 3},
+        {4, 5, 6}
+    };
+    std::vector<std::vector<int>> B = {
+        {7, 8},
+        {9, 10},
+        {11, 12}
+    };
+    std::vector<std::vector<int>> C(2, std::vector<int>(2, 0));
+
+    multiplyMatrices(A, B, C, 2, 3, 2);
+
+    std::vector<std::vector<int>> expected = {
+        {58, 64},
+        {139, 154}
+    };
+
+    ASSERT_EQ(C, expected) << "Matrix multiplication test failed! :(((()";
+}
+
+
 TEST(MatrixMultiplicationTest, TestMultiplyMatrices1) {//abbiamo tolto il lro primo test, da rimettere
     std::vector<std::vector<int>> A = {
         {1, 0,0},
